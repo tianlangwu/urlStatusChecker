4d367368e726df41a72425d91b71df6ccf4a94a1
Junyong Liu <jliu396@myseneca.ca>
2020-11-22 21:27:08 -0500

    add e2e tests

---
+++ test/e2e.test.js
@@ -0,0 +1,86 @@
+const execa = require("execa");
+const {describe, test, expect} = require("@jest/globals");
+
+describe("end-to-end integration", () => {
+  test("prints tool with no arguments pass", async () => {
+    const {stdout, stderr} = await execa("node urlstatuschecker");
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toEqual("");
+  });
+
+  test("prints tool version with arument -v", async () => {
+    const {stdout, stderr} = await execa("node urlstatuschecker -v");
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toEqual("");
+  });
+
+  test("prints url information with a path", async () => {
+    const {stdout, stderr} = await execa("node", [
+      "urlstatuschecker",
+      "tests/test1.html",
+    ]);
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toEqual("");
+  });
+
+  test("prints url information with two filepath", async () => {
+    const {stdout, stderr} = await execa("node", [
+      "urlstatuschecker",
+      "tests/test1.html",
+      "tests/test2.html",
+    ]);
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toEqual("");
+  });
+
+  test("prints url information with path and json output", async () => {
+    const {stdout, stderr} = await execa("node", [
+      "urlstatuschecker",
+      "-j",
+      "tests/test1.html",
+    ]);
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toEqual("");
+  });
+
+  test("prints url information with path and only good output", async () => {
+    const {stdout, stderr} = await execa("node", [
+      "urlstatuschecker",
+      "-g",
+      "tests/test1.html",
+    ]);
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toEqual("");
+  });
+
+  test("prints url information with path and only bad output", async () => {
+    const {stdout, stderr} = await execa("node", [
+      "urlstatuschecker",
+      "-b",
+      "tests/test1.html",
+    ]);
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toEqual("");
+  });
+
+  test("prints url information with url string", async () => {
+    const {stdout, stderr} = await execa("node", [
+      "urlstatuschecker",
+      "-u",
+      "https://www.google.com",
+    ]);
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toEqual("");
+  });
+
+  test("prints url information with ignore arugment", async () => {
+    const {stdout, stderr} = await execa("node", [
+      "urlstatuschecker",
+      "-i",
+      "tests/test1.html",
+      "tests/test2.html",
+    ]);
+    expect(stdout).toMatchSnapshot();
+    expect(stderr).toEqual("");
+  });
+});
